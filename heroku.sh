438fdb743c3d155a79a2ed377720d69631291981
Levent Gurses <levent.gurses@64clicks.com>
2013-01-02 01:24:18 -0500

    Added date info

--- heroku.sh
+++ heroku.sh
@@ -1,12 +1,18 @@
 rev=`git rev-parse HEAD`
-echo $rev
-pattern="s/[0-9a-f]\\b\{5,40\}\\b/$rev/"
-echo $pattern
+dt=`git log --pretty=format:"%cd" -1 --date=short`
 
-# Replace values
+# Replace revision
 cat public/config.json | sed "s/[0-9a-f]\{5,40\}/$rev/" > public/config-mod.json 
 cat public/config-mod.json > public/config.json
 rm -rf public/config-mod.json
+
+# Replace last edited date
+cat public/config.json | sed "s/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}/$dt/" > public/config-mod.json 
+cat public/config-mod.json > public/config.json
+rm -rf public/config-mod.json
+
+# Commit changes
+git commit public/config.json -m "Updated revision and date information"
 
 # Push to Github
 git push origin master
